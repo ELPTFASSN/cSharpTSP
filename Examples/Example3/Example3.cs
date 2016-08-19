a9e89a671e1c6d05fcf67d2e9ba70c6e4ae71808
pitufin <Hello @ whatever .com>
2016-08-19 00:11:00 -0300

    ex3 Final & cleaned

--- Examples/Example3/Example3.cs
+++ Examples/Example3/Example3.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Example3
 {
-    class Example3
+    internal class Example3
     {
         //entry point of program
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string consigna = "Ingresar un numero e indicar si es Positivo o Negativo";
+            var consigna = "Ingresar un numero e indicar si es Positivo o Negativo";
 
             int numero;
 
             Console.WriteLine(consigna);
-            
+
             #region interacciones
+
             //intercaccion con el usuario
             Console.WriteLine("\n\nIngresa un numero por favor: ");
             numero = Convert.ToInt16(Console.ReadLine());
@@ -28,28 +25,25 @@ namespace Example3
             {
                 Console.WriteLine("El numero ingresado : {0} , es positivo.", numero);
                 Console.ReadKey();
-                
-                
             }
             else
             {
                 Console.WriteLine("El numero ingresado : {0} no es positivo", numero);
                 Console.ReadKey();
-                
             }
 
             #endregion interacciones
 
-
             #region salida del programa
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\n\nPresiona cualquier tecla para salir por favor.");
             Console.ReadKey();
             Console.ResetColor();
             // cuando llega a este punto la ejecucion se cierra de manera adecuada
             Environment.Exit(0);
-            #endregion salida del programa
 
+            #endregion salida del programa
         }
     }
 }
